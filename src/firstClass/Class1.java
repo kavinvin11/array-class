4896534f907784c2e29edfd7674ae40bf05aff80
kavinvin11 <kavin1994kavi@gmail.com>
2021-10-27 14:44:06 +0530

    Before Edit

---
+++ src/firstClass/Class1.java
@@ -0,0 +1,13 @@
+package firstClass;
+
+public class Class1 {
+	public static void main(String[] args) {
+		int a[] = {1,2,3,4,5,6,7,8,9};
+		//for (int i:a) {
+		//for (int i=0;i<a.length;i++) {
+		//System.out.println(a[i]);
+		//System.out.println(i);
+		int b= a.length;
+		System.out.println(b);
+	}
+	}
